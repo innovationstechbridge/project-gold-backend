76aa4dc1bffc1b9aa3d8f2bbf4ffce87685e85ca
Abishek Khanal <45481516+abishek12@users.noreply.github.com>
2024-08-24 20:28:49 +0545

    route changed

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.status(200).json({
-        status: 200,
-        message: "server is up"
-    })
+app.get("/api", (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: "server is up",
+  });
 });
 
-app.listen(3000, () => {
-    console.log("Server is working");
+let server = app.listen(3000, () => {
+  console.log("Server is working");
+});
+
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Logged Error ${error}`);
+  server.close(() => process.exit(1));
 });
